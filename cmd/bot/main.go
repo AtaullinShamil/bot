b45ca73dc13024c6d15dc2caffa6cd523e673002
Шамиль Атауллин <shamil@MacBook-Air.local>
2023-03-27 06:19:58 +0300

    implement list commands

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/AtaullinShamil/bot/internal/service/product"
 	"github.com/joho/godotenv"
 )
 
@@ -30,6 +31,8 @@ func main() {
 		log.Panic(err)
 	}
 
+	productService := product.NewService()
+
 	for update := range updates {
 
 		if update.Message == nil { // If we got a message
@@ -40,7 +43,7 @@ func main() {
 		case "help":
 			helpCommand(bot, update.Message)
 		case "list":
-			listCommand(bot, update.Message)
+			listCommand(bot, update.Message, productService)
 		default:
 			defaultBehavior(bot, update.Message)
 		}
@@ -50,14 +53,21 @@ func main() {
 func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
 		"/help - help\n"+
-		"/list - list products",
+			"/list - list products",
 	)
 
 	bot.Send(msg)
 }
 
-func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "TBD")
+func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
+	outputMsgText := "Here all the products: \n\n"
+
+	products := productService.List()
+	for _, p := range products {
+		outputMsgText += p.Title
+		outputMsgText += "\n"
+	}
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsgText)
 	bot.Send(msg)
 }
 
